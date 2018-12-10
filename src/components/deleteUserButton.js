c2151395eb05db69e510c56ba209e7ce91bd732e
Clinton Johnson <dragoon1246224@gmail.com>
2018-12-10 11:57:02 -0500

    Made changes to delete button

--- src/components/deleteUserButton.js
+++ src/components/deleteUserButton.js
@@ -1,12 +1,9 @@
 import React from "react";
-import { Link } from 'react-router-dom';
 
 class DeleteUserButton extends React.Component {
   render() {
     return (
-      <Link to='/userProfile'><button className="profilePageNavButton"
-        onClick={this.setRedirect}
-      >Profile Page</button></Link>
+      <button className="deleteUserButton">Log Out</button>
     );
   }
 }
