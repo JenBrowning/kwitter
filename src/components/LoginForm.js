28b1dfc3afeee17f2c05b13577d99e87d1b7aefb
Jennifer Browning <browningjen@yahoo.com>
2018-12-12 13:06:56 -0500

    more bug fixes

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -33,67 +33,65 @@ class LoginForm extends Component {
 
   render() {
     return (
-      <form>
-        <div className="login-form">
-          <div class="ui middle aligned center aligned grid">
-            <div class="column">
-              <h1 class="ui image header">
-                <div class="content">Login To Your Account</div>
-              </h1>
-            </div>
+      <div className="login-form">
+        <div class="ui middle aligned center aligned grid">
+          <div class="column">
+            <h1 class="ui image header">
+              <div class="content">Login To Your Account</div>
+            </h1>
           </div>
+        </div>
 
-          <form
-            action="https://s.codepen.io/voltron2112/debug/PqrEPM?"
-            method="get"
-            class="ui large form"
-          >
-            <div class="ui stacked secondary segment">
-              <div class="field">
-                <div class="ui left icon input">
-                  <i class="user icon" />
-                  <input
-                    type="text"
-                    onChange={this.handleCheckUserName}
-                    required
-                    placeholder="Enter Username"
-                  />
-                </div>
+        <form
+          action="https://s.codepen.io/voltron2112/debug/PqrEPM?"
+          method="get"
+          class="ui large form"
+        >
+          <div class="ui stacked secondary segment">
+            <div class="field">
+              <div class="ui left icon input">
+                <i class="user icon" />
+                <input
+                  type="text"
+                  onChange={this.handleCheckUserName}
+                  required
+                  placeholder="Enter Username"
+                />
               </div>
-              <div class="field">
-                <div class="ui left icon input">
-                  <i class="lock icon" />
-                  <input
-                    type="password"
-                    onChange={this.handleCheckPassword}
-                    required
-                    placeholder="Enter Password"
-                  />
-                </div>
-              </div>
-              <Link to="/userProfile">
-                <div
-                  class="ui fluid large teal submit button"
-                  onClick={this.handleLogin}
-                >
-                  Login
-                </div>
-              </Link>
             </div>
-            <Link to="/registration">
-              <div class="ui message">
-                New To Us?{" "}
-                <a
-                  href="https://s.codepen.io/voltron2112/debug/PqrEPM?"
-                  onClick={this.setRedirect}
-                >
-                  Register
-                </a>
+            <div class="field">
+              <div class="ui left icon input">
+                <i class="lock icon" />
+                <input
+                  type="password"
+                  onChange={this.handleCheckPassword}
+                  required
+                  placeholder="Enter Password"
+                />
+              </div>
+            </div>
+            <Link to="/userProfile">
+              <div
+                class="ui fluid large teal submit button"
+                onClick={this.handleLogin}
+              >
+                Login
               </div>
             </Link>
-          </form>
-        </div>
-      </form>
+          </div>
+          <Link to="/registration">
+            <div class="ui message">
+              New To Us?{" "}
+              <a
+                href="https://s.codepen.io/voltron2112/debug/PqrEPM?"
+                onClick={this.setRedirect}
+              >
+                Register
+              </a>
+            </div>
+          </Link>
+        </form>
+      </div>
     );
   }
 }
--- src/components/mainFeedNavButton.js
+++ src/components/mainFeedNavButton.js
@@ -1,15 +1,17 @@
 import "../styles/mainFeedNavButton.css";
 import React from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 class MainFeedNavButton extends React.Component {
   render() {
     return (
       // button below takes user to the main page
-      // 
-      <Link to='/mainFeed'><button className="mainFeedNavButton"
-        onClick={this.setRedirect}
-      >Main Page</button></Link>
+      //
+      <Link to="/mainFeed">
+        <button className="mainFeedNavButton" onClick={this.setRedirect}>
+          Main Page
+        </button>
+      </Link>
     );
   }
 }
