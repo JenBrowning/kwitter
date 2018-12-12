c92aa37b111f900216279030a92e83b64795b74d
Jennifer Browning <browningjen@yahoo.com>
2018-12-12 12:27:57 -0500

    finished profile and redid login page semantic ui

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { login } from "../actions/action";
 import { Link } from "react-router-dom";
+// import { Form } from "semantic-ui-react";
+import "../styles/LoginForm.css";
 // Might need import for the link to RegistrationPage
 
 class LoginForm extends Component {
@@ -31,33 +33,67 @@ class LoginForm extends Component {
 
   render() {
     return (
-      <div className="login-form">
-        <form>
-          <fieldset>
-            <input
-              type="text"
-              onChange={this.handleCheckUserName}
-              required
-              placeholder="Enter username"
-            />
-          </fieldset>
-          <fieldset>
-            <input
-              type="password"
-              onChange={this.handleCheckPassword}
-              required
-              placeholder="Enter Password"
-            />
-          </fieldset>
-        </form>
-        <br />
-        <div>{this.props.result}</div>
-        <button onClick={this.handleLogIn}>Login</button>
+      <form>
+        <div className="login-form">
+          <div class="ui middle aligned center aligned grid">
+            <div class="column">
+              <h1 class="ui image header">
+                <div class="content">Login To Your Account</div>
+              </h1>
+            </div>
+          </div>
 
-        <Link to="/registration">
-          <button onClick={this.setRedirect}>New Users Sign up Here!</button>
-        </Link>
-      </div>
+          <form
+            action="https://s.codepen.io/voltron2112/debug/PqrEPM?"
+            method="get"
+            class="ui large form"
+          >
+            <div class="ui stacked secondary segment">
+              <div class="field">
+                <div class="ui left icon input">
+                  <i class="user icon" />
+                  <input
+                    type="text"
+                    onChange={this.handleCheckUserName}
+                    required
+                    placeholder="Enter Username"
+                  />
+                </div>
+              </div>
+              <div class="field">
+                <div class="ui left icon input">
+                  <i class="lock icon" />
+                  <input
+                    type="password"
+                    onChange={this.handleCheckPassword}
+                    required
+                    placeholder="Enter Password"
+                  />
+                </div>
+              </div>
+              <Link to="/userProfile">
+                <div
+                  class="ui fluid large teal submit button"
+                  onClick={this.handleLogin}
+                >
+                  Login
+                </div>
+              </Link>
+            </div>
+            <Link to="/registration">
+              <div class="ui message">
+                New To Us?{" "}
+                <a
+                  href="https://s.codepen.io/voltron2112/debug/PqrEPM?"
+                  onClick={this.setRedirect}
+                >
+                  Register
+                </a>
+              </div>
+            </Link>
+          </form>
+        </div>
+      </form>
     );
   }
 }
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,8 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { register, updateUser } from "../actions/action";
-import { Grid } from "semantic-ui-react";
-
+import { Grid, Card, Image, Icon } from "semantic-ui-react";
 import "../styles/Profile.css";
 //this was class changeuserprofile
 class Profile extends React.Component {
@@ -42,25 +41,84 @@ class Profile extends React.Component {
   render() {
     return (
       <Grid className="grid">
-        <Grid.Column className="gridcolumn1" width={3}>
-          <p>Grids Suck</p>
+        <Grid.Column className="gridcolumn1" width={4}>
+          <Card className="userprofile">
+            <Image src="https://semantic-ui.com/images/wireframe/image.png" />
+            <Card.Content>
+              <Card.Header>Jen</Card.Header>
+              <Card.Meta>
+                <span className="date">Joined in 2015</span>
+              </Card.Meta>
+              <Card.Description>
+                software engineer. mamabear. luv live music. jokes. chocolate.
+                swear2much. ambivert. trust your struggles.
+              </Card.Description>
+            </Card.Content>
+            <Card.Content extra>
+              <a>
+                <Icon name="user" />
+                1M Friends
+              </a>
+            </Card.Content>
+          </Card>
+          <div className="uploadphoto">
+            <button>Upload photo</button>
+          </div>
         </Grid.Column>
-        <Grid.Column className="gridcolumn2" width={7}>
-          <p>birfday</p>
+        <Grid.Column className="gridcolumn2" width={6}>
+          <p>Where the feed for Profile goes</p>
         </Grid.Column>
-        <Grid.Column className="gridcolumn3" width={3}>
-          <p>Coding sucks</p>
+        <Grid.Column className="gridcolumn3" width={4}>
+          <legend>Making a Change?</legend>
+          <fieldset className="fieldset">
+            <input
+              className="input"
+              type="text"
+              onChange={this.handleChangeUpdateDisplayName}
+              placeholder="New Display Name"
+            />
+          </fieldset>
+          <br />
+          <fieldset>
+            Enter New Password:
+            <br />
+            <input
+              className="input"
+              type="password"
+              onChange={this.handleChangeUpdatePassword}
+              placeholder="New Password"
+            />
+          </fieldset>
+          <br />
+          <fieldset>
+            Confirm New Password:
+            <br />
+            <input className="input" type="password" />
+          </fieldset>
+          <br />
+          <div className="submitchangebutton">
+            <button onClick={this.handleUpdate}>Submit changes</button>
+          </div>
+
+          <div className="deleteuserbutton">
+            <button>Delete user</button>
+          </div>
+          <div className="logoutbutton">
+            <button onClick={this.props.toLoginPage}>Log Out</button>
+          </div>
         </Grid.Column>
       </Grid>
     );
   }
 }
-// <Container className="container">
-//   <Grid className="gridprofilepage">
-//     <Grid.Row className="column1">
-//       <Grid.Column width={8}>
-//         <Container className="profilefeed" />
-//         {/* feedgoeshere */}
+{
+  /* <Container className="container">
+  <Grid className="gridprofilepage">
+    <Grid.Row className="column1">
+      <Grid.Column width={8}>
+        <Container className="profilefeed" />
+        {/* feedgoeshere */
+}
 //       </Grid.Column>
 //       <Grid.Row width={3}>
 //         <Card className="userprofile">
@@ -132,7 +190,7 @@ class Profile extends React.Component {
 //       </Grid.Column>
 //     </Grid.Row>
 //   </Grid>
-// </Container>
+// </Container> */}
 
 // ;  don't need map state to props because we aren't displaying the current values, therefore, nothing to read/display
 const mapDispatchToProps = dispatch => {
