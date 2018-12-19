71321b5ecc3929c7a502f5e2d002ef9d9bdbaf94
Lea Ellingwood <leaellingwood@hotmail.com>
2018-12-19 14:28:32 -0500

    changed link to createBrowserRouter

--- src/index.js
+++ src/index.js
@@ -10,24 +10,28 @@ import reducer from "./reducers/reducerIndex";
 import thunk from "redux-thunk";
 import { createBrowserHistory } from 'history';
 
-const browserHistory = createBrowserHistory()
-
+// const browserHistory = createBrowserHistory()
+const history = createBrowserHistory({basename: process.env.PUBLIC_URL})
 
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 const store = createStore(
-  connectRouter(browserHistory)(reducer),
-  composeEnhancers(applyMiddleware(routerMiddleware(browserHistory), thunk))
+  connectRouter(history)(reducer),
+  composeEnhancers(applyMiddleware(routerMiddleware(history), thunk))
 );
 
 ReactDOM.render(
   <Provider store={store}>
-    <ConnectedRouter basename={process.env.PUBLIC_URL} history={browserHistory}> 
+    <ConnectedRouter basename={process.env.PUBLIC_URL} history={history}> 
       <App />
     </ConnectedRouter>
   </Provider>,
   document.getElementById("root")
 );
 registerServiceWorker();
+
+
+
+// you'll need to update the createBrowserRouter call and pass it a correct basename value so it works on github pages
